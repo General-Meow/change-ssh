a2fe21f245faf051f3f668880dbc1c765bf91826
General-Meow <spl.ivalis@gmail.com>
2017-03-22 12:50:36 +0000

    dunno

--- wakeT2e.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/ash
-#to get more info you can type wakelan -?
-#wakelan -m 00031D0122F9
-wakelan -m 00031D069801
-
---
+++ wakeUpMachine.sh
@@ -0,0 +1,8 @@
+#!/bin/ash
+#to get more info you can type wakelan -?
+#wakelan -m 00031D0122F9
+wakelan -m 00031D069801
+
+#to wake up nuc using wakeonlan
+#wakeonlan is available in the ubuntu repo's
+wakeonlan b8:ae:ed:71:d3:72
